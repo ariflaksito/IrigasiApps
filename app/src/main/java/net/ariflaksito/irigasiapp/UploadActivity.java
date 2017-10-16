f0e9899a0538ac064251c6cd2e321b1cdcefe4c1
Arif Laksito <arif.laksito@gmail.com>
2017-10-16 20:49:02 +0700

    fix upload image

--- app/src/main/java/net/ariflaksito/irigasiapp/UploadActivity.java
+++ app/src/main/java/net/ariflaksito/irigasiapp/UploadActivity.java
@@ -10,18 +10,28 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import net.ariflaksito.lib.AndroidMultiPartEntity;
 import net.ariflaksito.models.Config;
 
-import net.ariflaksito.lib.RequestHandler;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 
 public class UploadActivity extends AppCompatActivity {
 
@@ -31,6 +41,7 @@ public class UploadActivity extends AppCompatActivity {
     private ImageView imgPreview;
     private Button btnUpload;
     public static final String UPLOAD_KEY = "image";
+    long totalSize = 0;
 
     private Bitmap bitmap;
 
@@ -77,7 +88,7 @@ public class UploadActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                uploadImage();
+                new UploadFileToServer().execute();
             }
         });
     }
@@ -89,8 +100,8 @@ public class UploadActivity extends AppCompatActivity {
 
         options.inSampleSize = 8;
 
-        //bitmap = BitmapFactory.decodeFile(filePath, options);
-        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.parse(filePath));
+        bitmap = BitmapFactory.decodeFile(filePath, options);
+        //bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.parse(filePath));
 
         imgPreview.setImageBitmap(bitmap);
 
@@ -101,45 +112,90 @@ public class UploadActivity extends AppCompatActivity {
         bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
         byte[] imageBytes = baos.toByteArray();
         String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
         return encodedImage;
     }
 
-    private void uploadImage(){
-        class UploadImage extends AsyncTask<Bitmap,Void,String> {
+    private class UploadFileToServer extends AsyncTask<Void, Integer, String> {
 
-            ProgressDialog loading;
-            RequestHandler rh = new RequestHandler();
+        ProgressDialog loading;
 
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-                loading = ProgressDialog.show(UploadActivity.this, "Uploading Image", "Please wait...",true,true);
-            }
-
-            @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
-                loading.dismiss();
-                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            protected String doInBackground(Bitmap... params) {
-                Bitmap bitmap = params[0];
-                String uploadImage = getStringImage(bitmap);
-
-                HashMap<String,String> data = new HashMap<>();
-                data.put(UPLOAD_KEY, uploadImage);
-
-                String result = rh.sendPostRequest(new Config().getUploadUrl(),data);
-
-                return result;
-            }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            loading = ProgressDialog.show(UploadActivity.this, "Uploading Image", "Please wait...",true,true);
         }
 
-        UploadImage ui = new UploadImage();
-        ui.execute(bitmap);
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            // Making progress bar visible
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            return uploadFile();
+        }
+
+        @SuppressWarnings("deprecation")
+        private String uploadFile() {
+            String responseString = null;
+
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(new Config().getUploadUrl());
+
+            try {
+                AndroidMultiPartEntity entity = new AndroidMultiPartEntity(
+                        new AndroidMultiPartEntity.ProgressListener() {
+
+                            @Override
+                            public void transferred(long num) {
+
+                            }
+                        });
+
+                File sourceFile = new File(filePath);
+
+                // Adding file data to http body
+                entity.addPart("image", new FileBody(sourceFile));
+
+                // Extra parameters if you want to pass to server
+                //entity.addPart("website",
+                //        new StringBody("www.androidhive.info"));
+                //entity.addPart("email", new StringBody("abc@gmail.com"));
+
+                totalSize = entity.getContentLength();
+                httppost.setEntity(entity);
+
+                // Making server call
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity r_entity = response.getEntity();
+
+                int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode == 200) {
+                    // Server response
+                    responseString = EntityUtils.toString(r_entity);
+                } else {
+                    responseString = "Error occurred! Http Status Code: "
+                            + statusCode;
+                }
+
+            } catch (ClientProtocolException e) {
+                responseString = e.toString();
+            } catch (IOException e) {
+                responseString = e.toString();
+            }
+
+            return responseString;
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.e("--irigasiApp--", "Response from server: " + result);
+            loading.dismiss();
+
+            super.onPostExecute(result);
+        }
+
     }
-
-
 }
